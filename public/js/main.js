85745a7d3269e132f97cc17e4798ae79fe6b7868
gekkeboyjeff <jeffrey.ullers@hva.nl>
2022-04-19 11:11:40 +0200

    added real basic chatroom with a few buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,17 @@
 //Register/update the service worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/serviceWorker.js')
-      .then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then(function (registration) {
         return registration.update();
-        })
-      });
-   }
+      })
+  });
+}
+
+document.querySelector('.plsChat').addEventListener('click', () => {
+  document.querySelector('.chatroom').style.setProperty('display', 'block')
+})
+
+document.querySelector('.chatroom button').addEventListener('click', () => {
+  document.querySelector('.chatroom').style.setProperty('display', 'none')
+})
