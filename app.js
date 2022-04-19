8fb598b4dbf5bf027367ae5e8514ddb2963852a9
Jeany de Vries <jeanydevries@hotmail.com>
2022-04-19 11:32:55 +0200

    removed rijksmuseum app

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const express = require('express');
-const path = require('path');
-const compression = require('compression')
-
-const app = express();
-const port = process.env.PORT || 3535;
-const api_key = "ixmhN4my&"
-
-// socket aanzetten
+const express = require('express')
+const app = express()
 const http = require('http').createServer(app)
+const path = require('path')
 const io = require('socket.io')(http)
+const port = process.env.PORT || 4242
 
-const fetch = (...args) => import('node-fetch').then(({
-  default: fetch
-}) => fetch(...args))
-
-//Let the app listen to ejs viewss
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/public/views'));
-
-//Set a public path, so /public is not needed anywhere
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(compression())
+app.use(express.static(path.resolve('public')))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
@@ -34,51 +19,6 @@ io.on('connection', (socket) => {
   })
 })
 
-app.get('/', function (req, res) {
-  fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=${api_key}&ps=20&imgonly=true`)
-    .then(async response => {
-      const artWorks = await response.json()
-      res.render('index', {
-        pageTitle: 'Home page',
-        data: artWorks.artObjects
-      })
-    })
-    .catch(err => res.send(err))
+http.listen(port, () => {
+  console.log('listening on port ', port)
 })
-
-app.get('/painting/:id', (req, res) => {
-  //Use the id in the request for the fetch
-  fetch(`https://www.rijksmuseum.nl/api/nl/collection/${req.params.id}?key=${api_key}&imgonly=true`)
-    .then(async response => {
-      const detail = await response.json()
-      res.render('detail', {
-        pageTitle: `Kunstwerk: ${req.params.id}`,
-        data: detail.artObject
-      })
-    })
-    .catch(err => res.send(err))
-})
-
-app.get('/search', (req, res) => {
-  //Use the query to get the search input
-  fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=${api_key}q=${req.query.query}`)
-    .then(async response => {
-      const search = await response.json()
-      res.render('searchResults', {
-        pageTitle: `Kunstwerk: ${req.query.query}`,
-        data: search.artObjects
-      })
-    })
-    .catch(err => {
-      res.send(err);
-    })
-})
-
-app.get('/offline', (req, res) => {
-  res.render('offline', {
-    pageTitle: `Offline`
-  });
-})
-
-
-http.listen(port);
--- public/main.js
+++ public/main.js
@@ -1,13 +1,3 @@
-//Register/update the service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/serviceWorker.js')
-      .then(function (registration) {
-        return registration.update();
-      })
-  });
-}
-
 document.querySelector('.plsChat').addEventListener('click', () => {
   document.querySelector('.chatroom').style.setProperty('display', 'block')
 })
