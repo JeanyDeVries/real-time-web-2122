47c18262af6fa29050eb71d515705b85128814c8
Jeany de Vries <jeanydevries@hotmail.com>
2022-04-22 10:17:30 +0200

    made container sort of responsive (not for mobile)

--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
 let socket = io()
 let messages = document.querySelector('.chatroom ul')
-let input = document.querySelector('input')
+//let input = document.querySelector('input')
 
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault()
-  if (input.value) {
-    socket.emit('message', input.value)
-    input.value = ''
-  }
+ // if (input.value) {
+  //  socket.emit('message', input.value)
+  //  input.value = ''
+  //}
 })
 
 socket.on('message', message => {
